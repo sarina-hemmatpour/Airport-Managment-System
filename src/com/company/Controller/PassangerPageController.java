00ef0e01dd266ae4deddb9ba8cb297e92e77320c
rolijesoo <blackpink.lisa.sh@gmail.com>
2020-08-11 02:10:44 +0430

    adding final passanger acount page controller and .fxml file

--- src/com/company/Controller/PassangerPageController.java
+++ src/com/company/Controller/PassangerPageController.java
@@ -1,15 +1,14 @@
 package com.company.Controller;
 
 import com.company.Main;
-import com.company.Model.Airplane;
-import com.company.Model.Flight;
-import com.company.Model.Ticket;
+import com.company.Model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -31,22 +30,29 @@ public class PassangerPageController implements Initializable {
     @FXML TextField raiseCreditTF;
     @FXML TabPane passangerPageTP;
     @FXML Button messageBTN;
+    @FXML Button buyBTN;
+    @FXML Button myTicketsBTN;
 
     //tableview part
-    @FXML TableColumn<Flight,String> originCLM;
-    @FXML TableColumn<Flight,String> destinationCLM;
-    @FXML TableColumn<Flight,String> dateCLM;
-    @FXML TableColumn<Flight,String> timeCLM;
-    @FXML TableColumn<Ticket,String> priceCLM;
-    @FXML TableColumn<Airplane,String> seatsCLM;
-    @FXML TableColumn ticketsCLM;
+    @FXML TableColumn originCLM;
+    @FXML TableColumn destinationCLM;
+    @FXML TableColumn dateCLM;
+    @FXML TableColumn timeCLM;
+
+//    @FXML TableView priceTV;
+    @FXML TableColumn priceCLM;
 
     static boolean prifileTabIsOpen=false;
     static boolean messagePageIsOpen=false;
+    static boolean flightsTabIsOpen=false;
 
-    private Tab profileTab;
 
-    private static TabPane passangerPageTPfake;
+    static Tab profileTab;
+    static Tab flightsTab;
+
+    public static TabPane passangerPageTPfake;
+
+    public static Label creditLBLfake;
 
     Stage messageStage;
 
@@ -57,18 +63,40 @@ public class PassangerPageController implements Initializable {
         passangerPageTPfake=passangerPageTP;
 
 
+        creditLBLfake=creditLBL;
+
         infoLBL.setText(Main.passangers.get(LoginPageController.loginUserIndex).getName()+
                 " "+Main.passangers.get(LoginPageController.loginUserIndex).getLastname());
         creditLBL.setText(Main.passangers.get(LoginPageController.loginUserIndex).getCredit());
-//        System.out.println(Main.passangers.get(LoginPageController.loginUserIndex).getCredit());
 
         //filling tableview
-//        originCLM.setCellValueFactory(new PropertyValueFactory<>("origin"));
-//        destinationCLM.setCellValueFactory(new PropertyValueFactory<>("destination"));
-//        dateCLM.setCellValueFactory(new PropertyValueFactory<>("date"));
-//        timeCLM.setCellValueFactory(new PropertyValueFactory<>("time"));
-//        priceCLM.setCellValueFactory(new PropertyValueFactory<>("price"));
-//        seatsCLM.setCellValueFactory(new PropertyValueFactory<>("seats"));
+        originCLM.setCellValueFactory(new PropertyValueFactory<>("origin"));
+        destinationCLM.setCellValueFactory(new PropertyValueFactory<>("desination"));
+        dateCLM.setCellValueFactory(new PropertyValueFactory<>("date"));
+        timeCLM.setCellValueFactory(new PropertyValueFactory<>("takeOffTime"));
+        priceCLM.setCellValueFactory(new PropertyValueFactory<>("flightPrice"));
+
+
+        System.out.println("size:"+Main.flights.size());
+        for (int i=0 ; i<Main.flights.size() ; i++)
+        {
+            if (Main.flights.get(i).getFlightStatus()== Flight.Status.notDONE)
+            {
+                flightsTV.getItems().add(Main.flights.get(i));
+            }
+
+        }
+
+
+
+//        for (int i=0 ; i<Main.flights.size() ; i++)
+//        {
+//            if (Main.flights.get(i).getFlightStatus()== Flight.Status.notDONE)
+//            {
+//                priceTV.getItems().add(Main.flights.get(i).getTicket());
+//            }
+//
+//        }
 
 
         //exit Button
@@ -117,6 +145,28 @@ public class PassangerPageController implements Initializable {
 
         });
 
+        myTicketsBTN.setOnAction(e -> {
+
+            if (!flightsTabIsOpen)
+            {
+                FXMLLoader loader=new FXMLLoader(Main.class.getResource("View/PassangerFlightPage.fxml"));
+                try {
+                    loader.load();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+                flightsTab=new Tab("Flights");
+                flightsTab.setStyle("-fx-background-color: slategray");
+                flightsTab.setContent(loader.getRoot());
+                passangerPageTP.getTabs().add(flightsTab);
+
+                flightsTabIsOpen=true;
+            }
+
+
+        });
+
         //messege button
         messageBTN.setOnAction(e ->{
 
@@ -129,6 +179,8 @@ public class PassangerPageController implements Initializable {
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
+                MessagePageController messagePageController=messageLoader.getController();
+                messagePageController.sender="Passanger";
 
                 messageStage=new Stage(StageStyle.UNDECORATED);
                 messageStage.setScene(new Scene(messageLoader.getRoot()));
@@ -140,13 +192,179 @@ public class PassangerPageController implements Initializable {
         });
 
 
+        buyBTN.setOnAction(e ->buyBTNaction(e));
+
 
     }
 
-    public static void closeTab(int i)
+
+    private void buyBTNaction(ActionEvent e)
     {
-        passangerPageTPfake.getTabs().remove(i);
+        if ( flightsTV.getSelectionModel().getSelectedItem() != null)
+        {
+            Flight flight=(Flight) flightsTV.getSelectionModel().getSelectedItem();
+            int indexInFlights=-1;
+            int indexInAirplane=-1;
+            int indexOfAirplane=-1;
+
+            for (int i=0 ; i<Main.airplanes.size() ; i++)
+            {
+                if (Main.airplanes.get(i).getId().equals(flight.getAirplane().getId()))
+                {
+                    indexOfAirplane=i;
+                    break;
+                }
+            }
+
+            for (int i=0 ; i<Main.airplanes.get(indexOfAirplane).getFlights().size() ; i++)
+            {
+                if (Main.airplanes.get(indexOfAirplane).getFlights().get(i).getId().equals(flight.getId()))
+                {
+                    indexInAirplane=i;
+                    break;
+                }
+            }
+
+            for (int i=0 ; i<Main.flights.size() ; i++)
+            {
+                if (Main.flights.get(i).getId().equals(flight.getId()))
+                {
+                    indexInFlights=i;
+                    break;
+                }
+            }
+            int maxValue=Integer.parseInt(flight.getAirplane().getSeats())-Integer.parseInt(flight.getNmbrOFsoldTickets());
+            if (flight.getFlightStatus()== Flight.Status.DONE)
+            {
+                warningLBL.setText("This flight is already Done!!!");
+            }
+            else if (maxValue==0)
+            {
+                //its full
+                warningLBL.setText("Sorry, full capacity!!!");
+            }
+            else if (checkInterfere((Flight) flightsTV.getSelectionModel().getSelectedItem()))
+            {
+                warningLBL.setText("This flight interferes with your other Flights");
+            }
+            else if (Integer.parseInt(flight.getTicket().getPrice()) >
+                    Integer.parseInt(Main.passangers.get(LoginPageController.loginUserIndex).getCredit()))
+            {
+                //low money
+                warningLBL.setText("Your credit is not enough!!!");
+            }
+            else
+            {
+
+                //every thing is ok
+                //lets buy
+
+                //pay for the ticket
+                //decrease credit
+                int oldCredit=Integer.parseInt(Main.passangers.get(LoginPageController.loginUserIndex).getCredit());
+                int flightPrice=Integer.parseInt(flight.getTicket().getPrice());
+                int newCredit=oldCredit-flightPrice;
+                String tempCredit="";
+                tempCredit+=newCredit;
+                Main.passangers.get(LoginPageController.loginUserIndex).setCredit(tempCredit);
+
+
+                //edit passangers ticket
+                Main.passangers.get(LoginPageController.loginUserIndex).getBaughtTickets().add(flight.getTicket());
+                //edit in file
+                WriteReadFile<Passanger> passangerWriteReadFile=new WriteReadFile<>( Main.passangers , "Passanger.txt");
+                passangerWriteReadFile.writeList();
+
+                for (int i=0 ; i<Main.airplanes.get(indexOfAirplane).getFlights().get(indexInAirplane).getPassangers().size() ; i++)
+                {
+                    System.out.println("before adding to the flight: " +
+                            Main.airplanes.get(indexOfAirplane).getFlights().get(indexInAirplane).getPassangers().get(i).getName());
+                }
+
+                //edit the flight
+
+                Main.flights.get(indexInFlights).getPassangers().add(Main.passangers.get(LoginPageController.loginUserIndex));
+
+                String tempSold="";
+                int tempInt=Integer.parseInt(Main.flights.get(indexInFlights).getNmbrOFsoldTickets())+1;
+                tempSold+=tempInt;
+                Main.flights.get(indexInFlights).setNmbrOFsoldTickets(tempSold);
+                //write in file
+                WriteReadFile<Flight> flightWriteReadFile = new WriteReadFile<>(Main.flights, "flights.txt");
+                flightWriteReadFile.writeList();
+
+
+
+                //edit in airplanes
+                for (int i=0 ; i<Main.airplanes.get(indexOfAirplane).getFlights().get(indexInAirplane).getPassangers().size() ; i++)
+                {
+                    System.out.println("before: " +
+                            Main.airplanes.get(indexOfAirplane).getFlights().get(indexInAirplane).getPassangers().get(i).getName());
+                }
+
+
+                if (Main.flights.get(indexInFlights)!=Main.airplanes.get(indexOfAirplane).getFlights().get(indexInAirplane))
+                {
+                    Main.airplanes.get(indexOfAirplane).getFlights().get(indexInAirplane).getPassangers()
+                            .add(Main.passangers.get(LoginPageController.loginUserIndex));
+
+                    Main.airplanes.get(indexOfAirplane).getFlights().get(indexInAirplane).setNmbrOFsoldTickets(tempSold);
+                }
+
+                //edit in file
+                WriteReadFile<Airplane> airplaneWriteReadFile = new WriteReadFile<>(Main.airplanes, "airplanes.txt");
+                airplaneWriteReadFile.writeList();
+
+                creditLBL.setText(Main.passangers.get(LoginPageController.loginUserIndex).getCredit());
+
+
+                warningLBL.setText("Done!!!");
+
+
+                //refresh the table
+                if (flightsTabIsOpen)
+                {
+                    PassangerFlightPageController.fakeFlightTV.getItems().clear();
+
+//                    PassangerFlightPageController.fakePriceTV.getItems().clear();
+
+                    for (int i=0 ; i< Main.passangers.get(LoginPageController.loginUserIndex).getBaughtTickets().size() ; i++)
+                    {
+                        for (int j=0 ; j<Main.flights.size() ; j++)
+                        {
+                            if (Main.passangers.get(LoginPageController.loginUserIndex).getBaughtTickets().get(i).getId()
+                                    .equals(Main.flights.get(j).getId()))
+                            {
+                                PassangerFlightPageController.fakeFlightTV.getItems().add(Main.flights.get(j));
+//                                PassangerFlightPageController.fakePriceTV.getItems().add(Main.flights.get(j).getTicket());
+                            }
+                        }
+                    }
+
+                    PassangerFlightPageController.fakeFlightTV.refresh();
+//                    PassangerFlightPageController.fakePriceTV.refresh();
+
+
+
+                    //record
+                    Record record=new Record();
+                    Main.records.add(record);
+                    record.passangerBuyTicket(Main.passangers.get(LoginPageController.loginUserIndex),Main.flights.get(indexInFlights).getTicket());
+                    //write in file
+                    WriteReadFile<Record> recordWriteReadFile=new WriteReadFile<>(Main.records , "Records.txt");
+                    recordWriteReadFile.writeList();
+
+                }
+            }
+        }
+        else
+        {
+            warningLBL.setText("Select a flight!!!");
+        }
     }
+
+
+
 
     private void raiseCreditBTNaction(ActionEvent e) throws IOException {
         if (raiseCreditTF.getText().isEmpty())
@@ -167,44 +385,44 @@ public class PassangerPageController implements Initializable {
                 add+=cash;
 
                 //edit file
-                String oldline=(Main.passangers.get(LoginPageController.loginUserIndex).getId()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getName()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getLastname()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getUsername()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getPassword()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getPhoneNumber()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getEmailAdress()+" "+
-                        Main.passangers.get(LoginPageController.loginUserIndex).getCredit());
-
-                String newLine=(Main.passangers.get(LoginPageController.loginUserIndex).getId()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getName()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getLastname()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getUsername()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getPassword()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getPhoneNumber()+" "
-                        +Main.passangers.get(LoginPageController.loginUserIndex).getEmailAdress()+" "
-                        +add);
+//                String oldline=(Main.passangers.get(LoginPageController.loginUserIndex).getId()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getName()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getLastname()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getUsername()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getPassword()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getPhoneNumber()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getEmailAdress()+" "+
+//                        Main.passangers.get(LoginPageController.loginUserIndex).getCredit());
+//
+//                String newLine=(Main.passangers.get(LoginPageController.loginUserIndex).getId()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getName()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getLastname()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getUsername()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getPassword()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getPhoneNumber()+" "
+//                        +Main.passangers.get(LoginPageController.loginUserIndex).getEmailAdress()+" "
+//                        +add);
 
                 //find the line
-                int lineToBeEdited=0;
-                FileReader fileReader=new FileReader("Passanger.txt");
-                BufferedReader bufferedReader=new BufferedReader(fileReader);
-
-                String line;
-                while ((line=bufferedReader.readLine()) != null)
-                {
-                    lineToBeEdited++;
-                    if (line==oldline)
-                    {
-                        break;
-                    }
-                }
-
-                String file="Passanger.txt";
-                ChangeLineInFile changeFile=new ChangeLineInFile();
-
-
-                changeFile.changeALineInATextFile(file,newLine,lineToBeEdited);
+//                int lineToBeEdited=0;
+//                FileReader fileReader=new FileReader("Passanger.txt");
+//                BufferedReader bufferedReader=new BufferedReader(fileReader);
+//
+//                String line;
+//                while ((line=bufferedReader.readLine()) != null)
+//                {
+//                    lineToBeEdited++;
+//                    if (line==oldline)
+//                    {
+//                        break;
+//                    }
+//                }
+//
+//                String file="Passanger.txt";
+//                ChangeLineInFile changeFile=new ChangeLineInFile();
+//
+//
+//                changeFile.changeALineInATextFile(file,newLine,lineToBeEdited);
 
 
                 //edit the object
@@ -212,12 +430,63 @@ public class PassangerPageController implements Initializable {
                 creditLBL.setText(Main.passangers.get(LoginPageController.loginUserIndex).getCredit());
                 warningLBL.setText("");
                 raiseCreditTF.setText("");
+
+                //edit file
+                WriteReadFile<Passanger> passangerWriteReadFile=new WriteReadFile<>(Main.passangers , "Passanger.txt");
+                passangerWriteReadFile.writeList();
+
+
+                Record record=new Record();
+                Main.records.add(record);
+                record.passangerRaiseCredit(Main.passangers.get(LoginPageController.loginUserIndex));
+                //write in file
+                WriteReadFile<Record> recordWriteReadFile=new WriteReadFile<>(Main.records , "Records.txt");
+                recordWriteReadFile.writeList();
             }
         }
     }
 
-    private void fillTableView()
-    {
 
+
+    private boolean checkInterfere(Flight choosenFlight)
+    {
+        for (int i=0 ; i<Main.passangers.get(LoginPageController.loginUserIndex).getBaughtTickets().size() ; i++)
+        {
+            //find the flight time for each ticket
+            int flightIndex=-1;
+            for (int j=0 ; j<Main.flights.size() ; j++)
+            {
+                if (Main.flights.get(j).getId().equals(Main.passangers.get(LoginPageController.loginUserIndex).getBaughtTickets().get(i).getId()))
+                {
+                    flightIndex=j;
+                }
+            }
+            System.out.println("flightindex: "+flightIndex);
+
+
+            if (Main.flights.get(flightIndex).getId().equals(choosenFlight.getId()))
+            {
+                return false;
+            }
+            else
+            {
+                if (!Main.flights.get(flightIndex).getDate().equals(choosenFlight.getDate()))
+                {
+                    return false;
+                }
+                else
+                {
+                    if (!Main.flights.get(flightIndex).getTakeOffTime().equals(choosenFlight.getTakeOffTime()))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
