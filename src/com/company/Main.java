ab95fcb4d43906176056685f54f837580279569d
rolijesoo <blackpink.lisa.sh@gmail.com>
2020-08-11 02:08:25 +0430

    adding Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import com.company.Controller.SuperAdminPageController;
+import com.company.Controller.WriteReadFile;
 import com.company.Model.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import javax.swing.text.DateFormatter;
+import javax.sound.midi.Soundbank;;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,64 +17,56 @@ import java.util.Locale;
 
 public class Main extends Application {
 
-
     public static Manager superAdmin ;
+
     public static ArrayList<Manager> managers=new ArrayList<>();
+
     public static ArrayList<Employee> employees=new ArrayList<>();
+
     public static ArrayList<Passanger> passangers=new ArrayList<>();
+
     public static ArrayList<Airplane> airplanes=new ArrayList<>();
+
     public static ArrayList<Ticket> tickets=new ArrayList<>();
+
     public static ArrayList<Flight> flights=new ArrayList<>();
+
     public static ArrayList<Message> messages=new ArrayList<>();
+
+    public static ArrayList<Record> records=new ArrayList<>();
 
     public static Stage loginPageStage;
 
 
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException , ClassNotFoundException {
 
-//        System.out.println(LocalDateTime.now());
-//
-//        DateTimeFormatter dtf =DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-//        LocalDateTime now=LocalDateTime.now();
-//        System.out.println(dtf.format(now));
+
+
+
+
+        System.out.println(LocalDateTime.now());
+
+        DateTimeFormatter dtf =DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now=LocalDateTime.now();
+        System.out.println(dtf.format(now));
 
         ////////////////////////////////////////////////////////////////////////////
-        //writing superAdmin in file
 
-        FileReader superAdminFR=new FileReader("SuperAdmin.txt");
-        BufferedReader superAdminBR=new BufferedReader(superAdminFR);
+//        //creating super admin
+//        superAdmin=new Manager("1743434421", "Sarina" , "Hemmatpour" ,"rolijesoo" ,
+//                "2221380" , "09140465914" , "blackpink.lisa.sh@gmail.com" , "8000" ,
+//                "Isfahan" , true);
 
-        FileWriter superAdminFW=new FileWriter("SuperAdmin.txt");
-        BufferedWriter superAdminBW=new BufferedWriter(superAdminFW);
+//        //save in file
+        WriteReadFile<Manager> superAdminWriteReadFile=new WriteReadFile<>(superAdmin,"SuperAdmin.txt");
+//        superAdminWriteReadFile.write();
 
-        if ((superAdminBR.readLine()) == null)
-        {
-            //the file is empty
-
+        superAdmin=superAdminWriteReadFile.read();
 
 
-            //saveing username and password of the super admin to SuperAdmin.txt
-            String forSave="1743434421 Sarina Hemmatpour rolijesoo 2221380 09140465914 " +
-                    "blackpink.lisa.sh@gmail.com 1000 Isfahan";
-            superAdminBW.write(forSave);
-            superAdminBW.newLine();
 
-            superAdminBW.close();
-        }
-
-        superAdminBW.close();
-
-        //super admin file isnt empty so we creat a superAdmin
-        //creat super admin
-
-        String toRead=superAdminBR.readLine();
-        String[] properties=toRead.split(" ");
-
-        superAdmin = new Manager(properties[0] , properties[1] , properties[2] , properties[3]
-                , properties[4] , properties[5] , properties[6] , properties[7]
-                , properties[8] , true );
         ////////////////////////////////////////////////////////////////////////////////////////////
 
         //saving all the users in array lists
@@ -80,78 +74,110 @@ public class Main extends Application {
         //reading files:
         ////////////////////////////////////////
         //managers:
-        FileReader managerFR=new FileReader("Managers.txt");
-        BufferedReader managerBR=new BufferedReader(managerFR);
+//        FileReader managerFR=new FileReader("Managers.txt");
+//        BufferedReader managerBR=new BufferedReader(managerFR);
+//
+//        String toRead1;
+//        while ((toRead1=managerBR.readLine()) != null)
+//        {
+//            String[] propeties=toRead1.split(" ");
+//
+//            Manager tempManager=new Manager(propeties[0] , propeties[1] , propeties[2] , propeties[3] , propeties[4]
+//                    , propeties[5] , propeties[6] , propeties[7] , propeties[8] , false);
+//
+//            managers.add(tempManager);
+//        }
+//        WriteReadFile<Manager> managerWriteReadFile=new WriteReadFile<>(managers , "Managers.txt");
+//        managers=managerWriteReadFile.readList();
 
-        String toRead1;
-        while ((toRead1=managerBR.readLine()) != null)
-        {
-            String[] propeties=toRead1.split(" ");
+        WriteReadFile<Manager> managerWriteReadFile = new WriteReadFile<>(managers, "Managers.txt");
+        managers = managerWriteReadFile.readList();
 
-            Manager tempManager=new Manager(propeties[0] , propeties[1] , propeties[2] , propeties[3] , propeties[4]
-                    , propeties[5] , propeties[6] , propeties[7] , propeties[8] , false);
 
-            managers.add(tempManager);
-        }
+
 
         //employees:
-        FileReader employeeFR=new FileReader("Employees.txt");
-        BufferedReader employeeBR=new BufferedReader(employeeFR);
+//        FileReader employeeFR=new FileReader("Employees.txt");
+//        BufferedReader employeeBR=new BufferedReader(employeeFR);
+//
+//        String toRead2;
+//        while ((toRead2=employeeBR.readLine()) != null)
+//        {
+//            String[] propeties=toRead2.split(" ");
+//
+//            Employee tempEmployee=new Employee(propeties[0] , propeties[1] , propeties[2] , propeties[3] , propeties[4]
+//                    , propeties[5] , propeties[6] , propeties[7] , propeties[8]);
+//
+//            employees.add(tempEmployee);
+//        }
 
-        String toRead2;
-        while ((toRead2=employeeBR.readLine()) != null)
-        {
-            String[] propeties=toRead2.split(" ");
-
-            Employee tempEmployee=new Employee(propeties[0] , propeties[1] , propeties[2] , propeties[3] , propeties[4]
-                    , propeties[5] , propeties[6] , propeties[7] , propeties[8]);
-
-            employees.add(tempEmployee);
-        }
+        WriteReadFile<Employee> employeeWriteReadFile=new WriteReadFile<>(employees,"Employees.txt");
+        employees=employeeWriteReadFile.readList();
 
         //passangers
-        FileReader passangerFR=new FileReader("Passanger.txt");
-        BufferedReader passangerBR=new BufferedReader(passangerFR);
+//        FileReader passangerFR=new FileReader("Passanger.txt");
+//        BufferedReader passangerBR=new BufferedReader(passangerFR);
+//
+//        String toRead3;
+//        while ((toRead3=passangerBR.readLine()) != null)
+//        {
+//            String[] propeties=toRead3.split(" ");
+//
+//            Passanger tempPassanger=new Passanger(propeties[0] , propeties[1] , propeties[2] , propeties[3] , propeties[4]
+//                    , propeties[5] , propeties[6] , propeties[7] );
+//
+//            passangers.add(tempPassanger);
+//        }
 
-        String toRead3;
-        while ((toRead3=passangerBR.readLine()) != null)
-        {
-            String[] propeties=toRead3.split(" ");
-
-            Passanger tempPassanger=new Passanger(propeties[0] , propeties[1] , propeties[2] , propeties[3] , propeties[4]
-                    , propeties[5] , propeties[6] , propeties[7] );
-
-            passangers.add(tempPassanger);
-        }
-
-
-        //messages
-        BufferedReader messageBR=new BufferedReader(new FileReader("Messages.txt"));
-
-        String toRead4;
-
-        while ((toRead4=messageBR.readLine()) != null)
-        {
-            String[] propertiesm=toRead4.split(";");
-
-
-            Message newMessage=new Message(propertiesm[1]);
-            newMessage.setTime(properties[0]);
-
-            messages.add(newMessage);
-        }
+        WriteReadFile<Passanger> passangerWriteReadFile=new WriteReadFile<>(passangers,"Passanger.txt");
+        passangers=passangerWriteReadFile.readList();
 
         //airplanes
 
+        WriteReadFile<Airplane> airplaneWriteReadFile =new WriteReadFile<>(airplanes , "airplanes.txt");
+        airplanes=airplaneWriteReadFile.readList();
+
+
+        //tickets
+        WriteReadFile<Ticket> ticketWriteReadFile=new WriteReadFile<>(tickets , "Tickets.txt");
+        tickets=ticketWriteReadFile.readList();
+
+        //flights
+        WriteReadFile<Flight> flightWriteReadFile=new WriteReadFile<>(flights,"flights.txt");
+        flights=flightWriteReadFile.readList();
+
+        //messages
+//        BufferedReader messageBR=new BufferedReader(new FileReader("Messages.txt"));
+//
+//        String toRead4;
+//
+//        while ((toRead4=messageBR.readLine()) != null)
+//        {
+//            String[] propertiesm=toRead4.split(";");
+//
+//
+//            Message newMessage=new Message(propertiesm[1]);
+//            newMessage.setTime(propertiesm[0]);
+//
+//            messages.add(newMessage);
+//        }
+        WriteReadFile<Message> messageWriteReadFile=new WriteReadFile<>(messages , "Messages.txt");
+        messages=messageWriteReadFile.readList();
+
+
+        //records
+        WriteReadFile<Record> recordWriteReadFile=new WriteReadFile<>(records , "Records.txt");
+        records=recordWriteReadFile.readList();
 
         ////////////////////////////////////////
 
-//        System.out.println(passangers.size());
+
+//        System.out.println(airplanes.get(0).getFlights().get(0).getPassangers().size());
 
 
 
 
-
+        setFlightsStatus();
         launch(args);
     }
 
@@ -175,5 +201,146 @@ public class Main extends Application {
         loginPageStage.setTitle("Login");
         loginPageStage.setScene(new Scene(loginPageLoader.getRoot()));
         loginPageStage.show();
+    }
+
+
+    private static void setFlightsStatus()
+    {
+        for (int i=0 ; i<flights.size() ; i++)
+        {
+            DateTimeFormatter dtf =DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now=LocalDateTime.now();
+            //joda krdane time haye now
+            String nowString=dtf.format(now);
+            String[] date_clockNow=nowString.split(" ");
+
+            String[] dateNow=date_clockNow[0].split("/");
+            String[] clockNow=date_clockNow[1].split(":");
+            //cast into integer
+            int yearNow=Integer.parseInt(dateNow[0])%1000;
+            int monthNow=Integer.parseInt(dateNow[1]);
+            int dayNow=Integer.parseInt(dateNow[2]);
+
+            int hourNow=Integer.parseInt(clockNow[0]);
+            int miniuteNow=Integer.parseInt(clockNow[1]);
+
+            //joda kardane flight time ha
+
+            String[] date=flights.get(i).getDate().split("/");
+            String[] clock=flights.get(i).getTakeOffTime().split(":");
+            //cast into integer
+            int year=Integer.parseInt(date[0]);
+            int month=Integer.parseInt(date[1]);
+            int day=Integer.parseInt(date[2]);
+
+            int hour=Integer.parseInt(clock[0]);
+            int miniute=Integer.parseInt(clock[1]);
+
+            //check
+            if (yearNow>year)
+            {
+                flights.get(i).setFlightStatus(Flight.Status.DONE);
+            }else if (year==yearNow)
+            {
+                if (monthNow>month)
+                {
+                    flights.get(i).setFlightStatus(Flight.Status.DONE);
+                }else if (monthNow==month)
+                {
+                    if (day<dayNow)
+                    {
+                        flights.get(i).setFlightStatus(Flight.Status.DONE);
+                    }else if(hour<hourNow)
+                    {
+                        flights.get(i).setFlightStatus(Flight.Status.DONE);
+                    }else if (hour==hourNow)
+                    {
+                        if (miniute<=miniuteNow)
+                        {
+                            flights.get(i).setFlightStatus(Flight.Status.DONE);
+                        }
+                    }
+                }
+            }
+        }
+        //save in file
+        WriteReadFile<Flight> flightWriteReadFile=new WriteReadFile<>(Main.flights,"flights.txt");
+        flightWriteReadFile.writeList();
+
+
+        for (int i=0 ; i<airplanes.size() ; i++)
+        {
+            for (int j=0 ; j<airplanes.get(i).getFlights().size() ; j++)
+            {
+                DateTimeFormatter dtf =DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                LocalDateTime now=LocalDateTime.now();
+                //joda krdane time haye now
+                String nowString=dtf.format(now);
+                String[] date_clockNow=nowString.split(" ");
+
+                String[] dateNow=date_clockNow[0].split("/");
+                String[] clockNow=date_clockNow[1].split(":");
+                //cast into integer
+                int yearNow=Integer.parseInt(dateNow[0])%1000;
+                int monthNow=Integer.parseInt(dateNow[1]);
+                int dayNow=Integer.parseInt(dateNow[2]);
+
+                int hourNow=Integer.parseInt(clockNow[0]);
+                int miniuteNow=Integer.parseInt(clockNow[1]);
+
+                //joda kardane flight time ha
+
+                String[] date=airplanes.get(i).getFlights().get(j).getDate().split("/");
+                String[] clock=airplanes.get(i).getFlights().get(j).getTakeOffTime().split(":");
+                //cast into integer
+                int year=Integer.parseInt(date[0]);
+                int month=Integer.parseInt(date[1]);
+                int day=Integer.parseInt(date[2]);
+
+                int hour=Integer.parseInt(clock[0]);
+                int miniute=Integer.parseInt(clock[1]);
+
+                //check
+                if (yearNow>year)
+                {
+                    airplanes.get(i).getFlights().get(j).setFlightStatus(Flight.Status.DONE);
+                }else if (year==yearNow)
+                {
+                    if (monthNow>month)
+                    {
+                        airplanes.get(i).getFlights().get(j).setFlightStatus(Flight.Status.DONE);
+                    }else if (monthNow==month)
+                    {
+                        if (day<dayNow)
+                        {
+                            airplanes.get(i).getFlights().get(j).setFlightStatus(Flight.Status.DONE);
+                        }else if (day==dayNow)
+                        {
+                            if(hour<hourNow)
+                            {
+                                airplanes.get(i).getFlights().get(j).setFlightStatus(Flight.Status.DONE);
+                            }else if (hour==hourNow)
+                            {
+                                if (miniute<=miniuteNow)
+                                {
+                                    airplanes.get(i).getFlights().get(j).setFlightStatus(Flight.Status.DONE);
+                                }
+                            }
+                        }
+
+
+
+                    }
+                }
+            }
+        }
+        //save in file
+        WriteReadFile<Airplane> airplaneWriteReadFile=new WriteReadFile<>(Main.airplanes,"airplanes.txt");
+        airplaneWriteReadFile.writeList();
+
+
+
+
+
     }
 }
